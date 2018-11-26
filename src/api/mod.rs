1b7cfb021e39b9b15dc6c9e6e14a76c5bb0e06a4
OCTronics <octronics@riseup.net>
2018-11-26 19:14:38 +0100

    API: Add `Validatable` trait
    Validate an object against the input device data and the exact device
    identification number.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,6 +3,8 @@
 // NOTE: Re-export the types to allow internal refactoring later.
 pub use self::convertible::Convertible;
 pub use self::error::Error;
+pub use self::validatable::Validatable;
 
 pub mod convertible;
 pub mod error;
+pub mod validatable;
---
+++ src/api/validatable.rs
@@ -0,0 +1,9 @@
+//! Validatable API
+
+use crate::{device::DeviceIn, types::DeviceId};
+
+/// Trait to validate an object around a given device
+pub trait Validatable {
+    /// Is this object valid for the given device?
+    fn is_valid_for(&self, id: &DeviceId, device: &DeviceIn);
+}
