374f527b155b3919d9c05167fb83f7ebd4e2f799
OCTronics <octronics@riseup.net>
2018-11-26 16:01:25 +0100

    main: Specify 'device' or 'info' for the 'show' command
    Not useful at it print the same data, but in preparation for next works.
    
    ```console
    cargo run -- show stm32f051R8T6 tests/stm32f051.yaml info
    ```

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,21 @@ use stm32builder::{DeviceId, Device};
 fn usage() {
     println!("Available commands:");
     println!("   decode <id>   - Decode an device identification number");
-    println!("   show <id> <device>");
+    println!("   show <id> <device> [device|info]");
     println!("                 - Show device informations from <device> file that match <id> device");
+    println!("                   Select 'device' to show all data (the default), 'info' for device informations only");
     println!("   help          - Print this message");
 }
 
 enum Cmd {
     Decode { id: DeviceId },
-    Show { id: DeviceId, device: File },
+    Show { id: DeviceId, device: File, data: Data },
     Help,
+}
+
+enum Data {
+    Device,
+    Info,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -38,9 +44,22 @@ fn main() -> Result<(), Box<dyn Error>> {
             );
             Ok(())
         }
-        Show { id, device } => {
+        Show { id, device, data } => {
             let device = Device::from_id_and_file(&id, &device)?;
-            Ok(println!("{:#?}", device))
+            match data {
+                Data::Device => Ok(println!("{:#?}", device)),
+                Data::Info => Ok(println!("{:#?}", device.info)),
+            }
+        }
+    }
+}
+
+impl Data {
+    pub fn from_arg(arg: &str) -> Result<Data, CliError> {
+        match arg {
+            "device" => Ok(Data::Device),
+            "info" => Ok(Data::Info),
+            arg => Err(CliError::UnknownDataArg(arg.to_owned())),
         }
     }
 }
@@ -61,6 +80,7 @@ impl Cmd {
             "show" => Ok(Show {
                 id: DeviceId::from_str(&args[2])?,
                 device: File::open(&args[3])?,
+                data: args.get(4).map_or(Ok(Data::Device), |arg| Data::from_arg(arg))?,
             }),
             cmd => Err(CliError::UnknownCommand(cmd.to_string()).into()),
         }
@@ -69,6 +89,7 @@ impl Cmd {
 
 #[derive(Debug)]
 enum CliError {
+    UnknownDataArg(String),
     UnknownCommand(String),
     NumberOfArguments(usize),
 }
@@ -79,6 +100,7 @@ impl Display for CliError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         use self::CliError::*;
         match *self {
+            UnknownDataArg(ref args) => write!(f, "data argument '{}' unknown", args),
             UnknownCommand(ref cmd) => write!(f, "command '{}' unknown", cmd),
             NumberOfArguments(ref num) => write!(f, "wrong number of arguments, {} given", num),
         }
